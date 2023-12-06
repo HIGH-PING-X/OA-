9b17879a157a40be0282f530e471de39d423dd5f
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-12-06 17:26:20 +0800

    Create mybatisPlusConfig.java

---
+++ oa/oa-utils/src/main/java/games/highping/utils/mybatisplus/mybatisPlusConfig.java
@@ -0,0 +1,21 @@
+package games.highping.utils.mybatisplus;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("games.highping.mapper")
+public class mybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
