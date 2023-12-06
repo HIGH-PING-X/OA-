f187a2de6870a11122b9b84a2b574b30423e9e4a
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-12-06 17:25:56 +0800

    Create SysRoleServiceImpl.java

---
+++ oa/oa-service/src/main/java/games/highping/service/impl/SysRoleServiceImpl.java
@@ -0,0 +1,22 @@
+package games.highping.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import games.highping.bean.SysRole;
+import games.highping.service.SysRoleService;
+import games.highping.mapper.SysRoleMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author HIGH-
+* @description 针对表【sys_role(角色)】的数据库操作Service实现
+* @createDate 2023-12-06 15:00:52
+*/
+@Service
+public class SysRoleServiceImpl extends ServiceImpl<SysRoleMapper, SysRole>
+    implements SysRoleService{
+
+}
+
+
+
+
