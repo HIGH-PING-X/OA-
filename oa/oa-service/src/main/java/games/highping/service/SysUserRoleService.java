5741b72530025d99ad7719a7820d736f55cb1ddf
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-12-07 17:24:04 +0800

    Create SysUserRoleService.java

---
+++ oa/oa-service/src/main/java/games/highping/service/SysUserRoleService.java
@@ -0,0 +1,13 @@
+package games.highping.service;
+
+import games.highping.bean.SysUserRole;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author HIGH-
+* @description 针对表【sys_user_role(用户角色)】的数据库操作Service
+* @createDate 2023-12-07 16:52:27
+*/
+public interface SysUserRoleService extends IService<SysUserRole> {
+
+}
