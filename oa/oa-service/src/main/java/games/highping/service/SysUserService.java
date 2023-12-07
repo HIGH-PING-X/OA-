8034fe8c19461bbfe4f4c54d15c4f02a5b8b7434
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-12-07 17:23:58 +0800

    Create SysUserService.java

---
+++ oa/oa-service/src/main/java/games/highping/service/SysUserService.java
@@ -0,0 +1,14 @@
+package games.highping.service;
+
+import games.highping.bean.SysUser;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author HIGH-
+* @description 针对表【sys_user(用户表)】的数据库操作Service
+* @createDate 2023-12-07 16:28:23
+*/
+public interface SysUserService extends IService<SysUser> {
+
+    void updateStatus(Long id, Integer status);
+}
