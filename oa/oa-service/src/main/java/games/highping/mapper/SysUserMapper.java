de0d5d2cab0948e0523372ba7ffdb9370c3b8f58
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-12-07 17:24:11 +0800

    Create SysUserMapper.java

---
+++ oa/oa-service/src/main/java/games/highping/mapper/SysUserMapper.java
@@ -0,0 +1,18 @@
+package games.highping.mapper;
+
+import games.highping.bean.SysUser;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author HIGH-
+* @description 针对表【sys_user(用户表)】的数据库操作Mapper
+* @createDate 2023-12-07 16:28:23
+* @Entity games.highping.bean.SysUser
+*/
+public interface SysUserMapper extends BaseMapper<SysUser> {
+
+}
+
+
+
+
