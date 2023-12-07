f8b7d7ef0b3f7bae90c2f179c30564a64b193ffa
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-12-07 17:24:19 +0800

    Update SysRoleController.java

--- oa/oa-service/src/main/java/games/highping/controller/SysRoleController.java
+++ oa/oa-service/src/main/java/games/highping/controller/SysRoleController.java
@@ -4,18 +4,18 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import games.highping.bean.SysRole;
+import games.highping.service.SysUserService;
 import games.highping.utils.result.Result;
 import games.highping.service.SysRoleService;
+import games.highping.utils.vo.AssginRoleVo;
 import games.highping.utils.vo.SysRoleQueryVo;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/admin/system/sysRole")
@@ -23,12 +23,21 @@ public class SysRoleController {
 
     @Autowired
     private SysRoleService sysRoleService;
+    @Autowired
+    private SysUserService sysUserService;
 
     @ApiOperation(value = "查询所有角色")
     @GetMapping("/findAll")
     public Result findAll() {
         List<SysRole> list = sysRoleService.list();
         return Result.ok(list);
+    }
+
+    @ApiOperation(value = "根据id查询角色")
+    @GetMapping("/get/{id}")
+    public Result get(@PathVariable Long id) {
+        SysRole sysRole = sysRoleService.getById(id);
+        return Result.ok(sysRole);
     }
 
     @ApiOperation(value = "分页查询角色")
@@ -46,8 +55,8 @@ public class SysRoleController {
     }
 
     @ApiOperation(value = "添加角色")
-    @GetMapping("/save")
-    public Result save(SysRole sysRole) {
+    @PostMapping("/save")
+    public Result save(@RequestBody SysRole sysRole) {
         boolean success = sysRoleService.save(sysRole);
         if (success) {
             return Result.ok();
@@ -55,5 +64,60 @@ public class SysRoleController {
             return Result.fail();
         }
     }
+
+    @ApiOperation(value = "修改角色")
+    @PutMapping("/update")
+    public Result updateById(@RequestBody SysRole sysRole) {
+        boolean success = sysRoleService.updateById(sysRole);
+        if (success) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
+    @ApiOperation(value = "根据id删除角色")
+    @DeleteMapping("/remove/{id}")
+    public Result remove(@PathVariable Long id) {
+        boolean success = sysRoleService.removeById(id);
+        if (success) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
+    @ApiOperation(value = "批量删除角色")
+    @DeleteMapping("/batchRemove")
+    public Result batchRemove(@RequestBody List<Long> idList) {
+        boolean success = sysRoleService.removeByIds(idList);
+        if (success) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
+    @ApiOperation("根据用户获取角色数据")
+    @GetMapping("/toAssign/{userId}")
+    public Result toAssign(@PathVariable Long userId) {
+        Map<String, Object> map = sysRoleService.findRoleDataByUserId(userId);
+        return Result.ok(map);
+    }
+
+    @ApiOperation("为用户分配角色")
+    @PostMapping("/doAssign")
+    public Result doAssign(@RequestBody AssginRoleVo assginRoleVo) {
+        sysRoleService.doAssign(assginRoleVo);
+        return Result.ok();
+    }
+
+    @ApiOperation(value = "更新状态")
+    @GetMapping("/updateStatus/{id}/{status}")
+    public Result updateStatus(@PathVariable Long id, @PathVariable Integer status){
+        sysUserService.updateStatus(id, status);
+        return Result.ok();
+    }
+
 
 }
