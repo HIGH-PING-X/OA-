36ae9a3dd047a57f00156b036619470564164eb7
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-12-08 17:24:45 +0800

    Create SysMenuController.java

---
+++ oa/oa-service/src/main/java/games/highping/controller/SysMenuController.java
@@ -0,0 +1,49 @@
+package games.highping.controller;
+
+import games.highping.bean.SysMenu;
+import games.highping.service.SysMenuService;
+import games.highping.utils.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Api(tags = "菜单管理")
+@RestController
+@RequestMapping("/admin/system/sysMenu")
+public class SysMenuController {
+
+    @Autowired
+    private SysMenuService sysMenuService;
+
+    @ApiOperation(value = "查询所有菜单")
+    @GetMapping("/findNodes")
+    public Result findNodes() {
+        List<SysMenu> list = sysMenuService.findNodes();
+        return Result.ok(list);
+    }
+
+    @ApiOperation(value = "新增菜单")
+    @PostMapping("save")
+    public Result save(@RequestBody SysMenu sysMenu) {
+        sysMenuService.save(sysMenu);
+        return Result.ok();
+    }
+
+    @ApiOperation(value = "修改菜单")
+    @PutMapping("update")
+    public Result updateById(@RequestBody SysMenu sysMenu) {
+        sysMenuService.updateById(sysMenu);
+        return Result.ok();
+    }
+
+    @ApiOperation(value = "删除菜单")
+    @DeleteMapping("remove/{id}")
+    public Result remove(@PathVariable Long id) {
+        sysMenuService.removeMenuById(id);
+        return Result.ok();
+    }
+
+}
