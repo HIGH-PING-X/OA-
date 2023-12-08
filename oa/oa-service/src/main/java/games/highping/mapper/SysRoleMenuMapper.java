64f0683530f854a3d35eb196d2ec9eb2c4acb8d6
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-12-08 17:25:10 +0800

    Create SysRoleMenuMapper.java

---
+++ oa/oa-service/src/main/java/games/highping/mapper/SysRoleMenuMapper.java
@@ -0,0 +1,18 @@
+package games.highping.mapper;
+
+import games.highping.bean.SysRoleMenu;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author HIGH-
+* @description 针对表【sys_role_menu(角色菜单)】的数据库操作Mapper
+* @createDate 2023-12-08 16:44:18
+* @Entity games.highping.bean.SysRoleMenu
+*/
+public interface SysRoleMenuMapper extends BaseMapper<SysRoleMenu> {
+
+}
+
+
+
+
