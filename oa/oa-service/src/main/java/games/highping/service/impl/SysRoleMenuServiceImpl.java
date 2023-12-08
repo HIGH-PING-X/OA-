ca90c03b9d8bc1c715f1fd68ed8baff961aca65a
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-12-08 17:25:00 +0800

    Create SysRoleMenuServiceImpl.java

---
+++ oa/oa-service/src/main/java/games/highping/service/impl/SysRoleMenuServiceImpl.java
@@ -0,0 +1,21 @@
+package games.highping.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import games.highping.bean.SysRoleMenu;
+import games.highping.service.SysRoleMenuService;
+import games.highping.mapper.SysRoleMenuMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author HIGH-
+* @description 针对表【sys_role_menu(角色菜单)】的数据库操作Service实现
+* @createDate 2023-12-08 16:44:18
+*/
+@Service
+public class SysRoleMenuServiceImpl extends ServiceImpl<SysRoleMenuMapper, SysRoleMenu> implements SysRoleMenuService{
+
+}
+
+
+
+
