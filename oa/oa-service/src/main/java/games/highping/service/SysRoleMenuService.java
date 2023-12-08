916adaaa819cb3e6d9dc10f5887fdbc38f0e4677
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-12-08 17:24:58 +0800

    Create SysRoleMenuService.java

---
+++ oa/oa-service/src/main/java/games/highping/service/SysRoleMenuService.java
@@ -0,0 +1,13 @@
+package games.highping.service;
+
+import games.highping.bean.SysRoleMenu;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author HIGH-
+* @description 针对表【sys_role_menu(角色菜单)】的数据库操作Service
+* @createDate 2023-12-08 16:44:18
+*/
+public interface SysRoleMenuService extends IService<SysRoleMenu> {
+
+}
