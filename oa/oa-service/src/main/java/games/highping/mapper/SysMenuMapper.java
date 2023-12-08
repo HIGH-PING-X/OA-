e8bde740f8edfb6619466346d445b7db3a123c55
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-12-08 17:24:52 +0800

    Create SysMenuMapper.java

---
+++ oa/oa-service/src/main/java/games/highping/mapper/SysMenuMapper.java
@@ -0,0 +1,18 @@
+package games.highping.mapper;
+
+import games.highping.bean.SysMenu;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author HIGH-
+* @description 针对表【sys_menu(菜单表)】的数据库操作Mapper
+* @createDate 2023-12-08 16:44:05
+* @Entity games.highping.bean.SysMenu
+*/
+public interface SysMenuMapper extends BaseMapper<SysMenu> {
+
+}
+
+
+
+
