1436c04145c7f3a8d0e89abf209c187b11e4936e
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-12-09 17:27:19 +0800

    Update SysMenuServiceImpl.java

--- oa/oa-service/src/main/java/games/highping/service/impl/SysMenuServiceImpl.java
+++ oa/oa-service/src/main/java/games/highping/service/impl/SysMenuServiceImpl.java
@@ -3,12 +3,20 @@ package games.highping.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import games.highping.bean.SysMenu;
+import games.highping.bean.SysRoleMenu;
+import games.highping.mapper.SysRoleMenuMapper;
 import games.highping.service.SysMenuService;
 import games.highping.mapper.SysMenuMapper;
+import games.highping.service.SysRoleMenuService;
 import games.highping.utils.MenuHelper;
+import games.highping.utils.vo.AssginMenuVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
 * @author HIGH-
@@ -17,6 +25,9 @@ import java.util.List;
 */
 @Service
 public class SysMenuServiceImpl extends ServiceImpl<SysMenuMapper, SysMenu> implements SysMenuService{
+
+    @Autowired
+    private SysRoleMenuMapper sysRoleMenuMapper;
 
     @Override
     public List<SysMenu> findNodes() {
@@ -34,6 +45,42 @@ public class SysMenuServiceImpl extends ServiceImpl<SysMenuMapper, SysMenu> impl
             throw new RuntimeException("该菜单下存在子菜单，无法删除");
         }
         baseMapper.deleteById(id);
+    }
+
+    @Override
+    public List<SysMenu> findSysMenuByRoleId(Long roleId) {
+        //全部权限列表
+        List<SysMenu> allSysMenuList = this.list(new LambdaQueryWrapper<SysMenu>().eq(SysMenu::getStatus, 1));
+
+        //根据角色id获取角色权限
+        List<SysRoleMenu> sysRoleMenuList = sysRoleMenuMapper.selectList(new LambdaQueryWrapper<SysRoleMenu>().eq(SysRoleMenu::getRoleId, roleId));
+        //转换给角色id与角色权限对应Map对象
+        List<Long> menuIdList = sysRoleMenuList.stream().map(e -> e.getMenuId()).collect(Collectors.toList());
+
+        allSysMenuList.forEach(permission -> {
+            if (menuIdList.contains(permission.getId())) {
+                permission.setSelect(true);
+            } else {
+                permission.setSelect(false);
+            }
+        });
+
+        List<SysMenu> sysMenuList = MenuHelper.buildTree(allSysMenuList);
+        return sysMenuList;
+    }
+
+    @Transactional
+    @Override
+    public void doAssign(AssginMenuVo assginMenuVo) {
+        sysRoleMenuMapper.delete(new LambdaQueryWrapper<SysRoleMenu>().eq(SysRoleMenu::getRoleId, assginMenuVo.getRoleId()));
+
+        for (Long menuId : assginMenuVo.getMenuIdList()) {
+            if (StringUtils.isEmpty(menuId)) continue;
+            SysRoleMenu rolePermission = new SysRoleMenu();
+            rolePermission.setRoleId(assginMenuVo.getRoleId());
+            rolePermission.setMenuId(menuId);
+            sysRoleMenuMapper.insert(rolePermission);
+        }
     }
 }
 
