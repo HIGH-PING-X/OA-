b449f7094089f726e1b4a29feb942344be78cffd
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-12-11 17:21:15 +0800

    Update ResultCodeEnum.java

--- oa/oa-utils/src/main/java/games/highping/utils/result/ResultCodeEnum.java
+++ oa/oa-utils/src/main/java/games/highping/utils/result/ResultCodeEnum.java
@@ -7,10 +7,10 @@ public enum ResultCodeEnum {
 
     SUCCESS(200, "成功"),
     FAIL(201, "失败"),
-    server_error(500, "服务器异常"),
-    data_error(201, "数据异常"),
-    login_error(208, "登录失败"),
-    permission_error(209, "权限不足");
+    SERVER_ERROR(500, "服务器异常"),
+    DATA_ERROR(201, "数据异常"),
+    LOGIN_ERROR(208, "登录失败"),
+    PERMISSION_ERROR(209, "权限不足");
 
     private Integer code;
     private String message;
