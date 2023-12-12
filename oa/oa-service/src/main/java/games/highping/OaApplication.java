76752c756fe0813bd0db13c3f0dc21e42372e9df
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-12-12 17:12:12 +0800

    Update OaApplication.java

--- oa/oa-service/src/main/java/games/highping/OaApplication.java
+++ oa/oa-service/src/main/java/games/highping/OaApplication.java
@@ -2,10 +2,8 @@ package games.highping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"games.highping"})
 public class OaApplication {
 
     public static void main(String[] args) {
