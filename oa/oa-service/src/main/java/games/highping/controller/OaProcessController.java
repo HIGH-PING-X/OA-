ebbdddbf94b2be77e240b97e4ef62ea4f603ce71
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-12-12 17:30:15 +0800

    Update OaProcessController.java

--- oa/oa-service/src/main/java/games/highping/controller/OaProcessController.java
+++ oa/oa-service/src/main/java/games/highping/controller/OaProcessController.java
@@ -1,8 +1,34 @@
 package games.highping.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import games.highping.bean.OaProcess;
+import games.highping.service.OaProcessService;
+import games.highping.utils.result.Result;
+import games.highping.utils.vo.ProcessQueryVo;
+import games.highping.utils.vo.ProcessVo;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.activiti.bpmn.model.Process;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("/admin/process")
 public class OaProcessController {
+
+    @Autowired
+    private OaProcessService oaProcessService;
+
+    //@PreAuthorize("hasAuthority('bnt.process.list')")
+    @ApiOperation(value = "获取分页列表")
+    @GetMapping("{page}/{limit}")
+    public Result index(@PathVariable Long page, @PathVariable Long limit, ProcessQueryVo processQueryVo) {
+        Page<OaProcess> pageParam = new Page<>(page, limit);
+        return Result.ok(oaProcessService.selectPage(pageParam, processQueryVo));
+    }
+
 }
