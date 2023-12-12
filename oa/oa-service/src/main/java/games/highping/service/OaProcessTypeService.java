08142b3eba4175481a5f425c83466a960a053230
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-12-12 17:11:58 +0800

    Create OaProcessTypeService.java

---
+++ oa/oa-service/src/main/java/games/highping/service/OaProcessTypeService.java
@@ -0,0 +1,13 @@
+package games.highping.service;
+
+import games.highping.bean.OaProcessType;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author HIGH-
+* @description 针对表【oa_process_type(审批类型)】的数据库操作Service
+* @createDate 2023-12-12 14:57:49
+*/
+public interface OaProcessTypeService extends IService<OaProcessType> {
+
+}
