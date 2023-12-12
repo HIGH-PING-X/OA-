5897afbc70ad8aeb6d9493282fa0693760dd5fd9
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-12-12 17:12:01 +0800

    Create OaProcessServiceImpl.java

---
+++ oa/oa-service/src/main/java/games/highping/service/impl/OaProcessServiceImpl.java
@@ -0,0 +1,22 @@
+package games.highping.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import games.highping.bean.OaProcess;
+import games.highping.service.OaProcessService;
+import games.highping.mapper.OaProcessMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author HIGH-
+* @description 针对表【oa_process(审批类型)】的数据库操作Service实现
+* @createDate 2023-12-12 15:40:02
+*/
+@Service
+public class OaProcessServiceImpl extends ServiceImpl<OaProcessMapper, OaProcess>
+    implements OaProcessService{
+
+}
+
+
+
+
