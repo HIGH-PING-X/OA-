f0cfd855a0f4c157bfbfd523de7f6256c16afa6e
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-12-12 17:12:26 +0800

    Create OaProcessTemplateMapper.java

---
+++ oa/oa-service/src/main/java/games/highping/mapper/OaProcessTemplateMapper.java
@@ -0,0 +1,18 @@
+package games.highping.mapper;
+
+import games.highping.bean.OaProcessTemplate;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author HIGH-
+* @description 针对表【oa_process_template(审批模板)】的数据库操作Mapper
+* @createDate 2023-12-12 14:57:38
+* @Entity games.highping.bean.OaProcessTemplate
+*/
+public interface OaProcessTemplateMapper extends BaseMapper<OaProcessTemplate> {
+
+}
+
+
+
+
