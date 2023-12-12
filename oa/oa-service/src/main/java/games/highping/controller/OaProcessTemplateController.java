e0e575c97ce58a08b3541342099774db46c488b9
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-12-12 17:12:08 +0800

    Create OaProcessTemplateController.java

---
+++ oa/oa-service/src/main/java/games/highping/controller/OaProcessTemplateController.java
@@ -0,0 +1,107 @@
+package games.highping.controller;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import games.highping.bean.OaProcessTemplate;
+import games.highping.service.OaProcessTemplateService;
+import games.highping.utils.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Api(tags = "审批模板管理")
+@RestController
+@RequestMapping(value = "/admin/process/processTemplate")
+@SuppressWarnings({"unchecked", "rawtypes"})
+public class OaProcessTemplateController {
+
+    @Autowired
+    private OaProcessTemplateService oaProcessTemplateService;
+
+    //@PreAuthorize("hasAuthority('bnt.processTemplate.list')")
+    @ApiOperation(value = "获取分页列表")
+    @GetMapping("{page}/{limit}")
+    public Result index(@ApiParam(name = "page", value = "当前页码", required = true)
+                        @PathVariable Long page,
+                        @ApiParam(name = "limit", value = "每页记录数", required = true)
+                        @PathVariable Long limit) {
+        Page<OaProcessTemplate> pageParam = new Page<>(page, limit);
+        IPage<OaProcessTemplate> pageModel = oaProcessTemplateService.selectPage(pageParam);
+        return Result.ok(pageModel);
+    }
+
+    //@PreAuthorize("hasAuthority('bnt.processTemplate.list')")
+    @ApiOperation(value = "获取")
+    @GetMapping("get/{id}")
+    public Result get(@PathVariable Long id) {
+        OaProcessTemplate processTemplate = oaProcessTemplateService.getById(id);
+        return Result.ok(processTemplate);
+    }
+
+    //@PreAuthorize("hasAuthority('bnt.processTemplate.templateSet')")
+    @ApiOperation(value = "新增")
+    @PostMapping("save")
+    public Result save(@RequestBody OaProcessTemplate processTemplate) {
+        oaProcessTemplateService.save(processTemplate);
+        return Result.ok();
+    }
+
+    //@PreAuthorize("hasAuthority('bnt.processTemplate.templateSet')")
+    @ApiOperation(value = "修改")
+    @PutMapping("update")
+    public Result updateById(@RequestBody OaProcessTemplate processTemplate) {
+        oaProcessTemplateService.updateById(processTemplate);
+        return Result.ok();
+    }
+
+    //@PreAuthorize("hasAuthority('bnt.processTemplate.remove')")
+    @ApiOperation(value = "删除")
+    @DeleteMapping("remove/{id}")
+    public Result remove(@PathVariable Long id) {
+        oaProcessTemplateService.removeById(id);
+        return Result.ok();
+    }
+
+    //@PreAuthorize("hasAuthority('bnt.processTemplate.templateSet')")
+    @ApiOperation(value = "上传流程定义")
+    @PostMapping("/uploadProcessDefinition")
+    public Result uploadProcessDefinition(MultipartFile file) throws FileNotFoundException {
+        String path = new File(ResourceUtils.getURL("classpath:").getPath()).getAbsolutePath();
+        File tempFile = new File(path + "/process/");
+        if (!tempFile.exists()) {
+            tempFile.mkdirs();
+        }
+        String fileName = file.getOriginalFilename();
+        File zipFile = new File(path + "/process/" + fileName);
+        try {
+            file.transferTo(zipFile);
+        } catch (IOException e) {
+            return Result.fail("上传失败");
+        }
+        Map<String, Object> map = new HashMap<>();
+        //根据上传地址后续部署流程定义，文件名称为流程定义的默认key
+        map.put("processDefinitionPath", "processes/" + fileName);
+        map.put("processDefinitionKey", fileName.substring(0, fileName.lastIndexOf(".")));
+        return Result.ok(map);
+    }
+
+    //@PreAuthorize("hasAuthority('bnt.processTemplate.publish')")
+    @ApiOperation(value = "发布")
+    @GetMapping("/publish/{id}")
+    public Result publish(@PathVariable Long id) {
+        oaProcessTemplateService.publish(id);
+        return Result.ok();
+    }
+
+}
