87cc930d8f1ace81a2e17cafc4e33a8abae38827
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-12-12 17:12:00 +0800

    Create OaProcessTypeServiceImpl.java

---
+++ oa/oa-service/src/main/java/games/highping/service/impl/OaProcessTypeServiceImpl.java
@@ -0,0 +1,21 @@
+package games.highping.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import games.highping.bean.OaProcessType;
+import games.highping.service.OaProcessTypeService;
+import games.highping.mapper.OaProcessTypeMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author HIGH-
+* @description 针对表【oa_process_type(审批类型)】的数据库操作Service实现
+* @createDate 2023-12-12 14:57:49
+*/
+@Service
+public class OaProcessTypeServiceImpl extends ServiceImpl<OaProcessTypeMapper, OaProcessType> implements OaProcessTypeService{
+
+}
+
+
+
+
