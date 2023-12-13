2d78bd85fa3dbd1fdc74c063ab2aac9259fc27b6
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-12-13 14:37:55 +0800

    Update OaProcessServiceImpl.java

--- oa/oa-service/src/main/java/games/highping/service/impl/OaProcessServiceImpl.java
+++ oa/oa-service/src/main/java/games/highping/service/impl/OaProcessServiceImpl.java
@@ -1,12 +1,19 @@
 package games.highping.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import games.highping.bean.OaProcess;
 import games.highping.service.OaProcessService;
 import games.highping.mapper.OaProcessMapper;
 import games.highping.utils.vo.ProcessQueryVo;
+import games.highping.utils.vo.ProcessVo;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.repository.Deployment;
 import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.util.zip.ZipInputStream;
 
 /**
 * @author HIGH-
@@ -16,9 +23,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class OaProcessServiceImpl extends ServiceImpl<OaProcessMapper, OaProcess> implements OaProcessService{
 
+    private RepositoryService repositoryService;
+
     @Override
-    public Object selectPage(Page<OaProcess> pageParam, ProcessQueryVo processQueryVo) {
-        Page<OaProcess> pageModel = baseMapper.selectPage(pageParam, processQueryVo);
+    public IPage<ProcessVo> selectPage(Page<ProcessVo> pageParam, ProcessQueryVo processQueryVo) {
+        IPage<ProcessVo> pageModel = baseMapper.selectPage(pageParam, processQueryVo);
+        return pageModel;
+    }
+
+    @Override
+    public void deployByZip(String deployPath) {
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(deployPath);
+        ZipInputStream zipInputStream = new ZipInputStream(inputStream);
+        Deployment deployment = repositoryService.createDeployment().addZipInputStream(zipInputStream).deploy();
     }
 }
 
