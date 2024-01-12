58f0a8ca66d8c8967f88d7e67e25688003a1acc5
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2024-01-12 17:11:04 +0800

    Create OaProcessRecordMapper.java

---
+++ oa/oa-service/src/main/java/games/highping/mapper/OaProcessRecordMapper.java
@@ -0,0 +1,18 @@
+package games.highping.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import games.highping.bean.OaProcessRecord;
+
+/**
+* @author HIGH-
+* @description 针对表【oa_process_record(审批记录)】的数据库操作Mapper
+* @createDate 2024-01-04 16:53:11
+* @Entity games.highping.bean.OaProcessRecord
+*/
+public interface OaProcessRecordMapper extends BaseMapper<OaProcessRecord> {
+
+}
+
+
+
+
