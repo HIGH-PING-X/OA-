04609e9bf49d02c7d85943f58bed36aeae5d3552
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2024-01-12 17:10:49 +0800

    Update ProcessController.java

--- oa/oa-service/src/main/java/games/highping/controller/ProcessController.java
+++ oa/oa-service/src/main/java/games/highping/controller/ProcessController.java
@@ -1,29 +1,58 @@
 package games.highping.controller;
 
-import games.highping.bean.OaProcessType;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import games.highping.bean.OaProcess;
+import games.highping.bean.OaProcessTemplate;
+import games.highping.service.OaProcessService;
+import games.highping.service.OaProcessTemplateService;
 import games.highping.service.OaProcessTypeService;
 import games.highping.utils.result.Result;
+import games.highping.utils.vo.ProcessFormVo;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @Api(tags = "审批流管理")
 @RestController
 @RequestMapping("/admin/process")
-@CrossOrigin // 跨域
+@CrossOrigin //跨域
 public class ProcessController {
 
-//    @Autowired
-//    private OaProcessTypeService oaProcessTypeService;
-//
-//    @GetMapping("/findProcessType")
-//    public Result findProcessType() {
-//        List<OaProcessType> list = oaProcessTypeService.findProcessType();
-//        return Result.ok();
-//    }
+    @Autowired
+    private OaProcessTypeService oaProcessTypeService;
+    @Autowired
+    private OaProcessTemplateService oaProcessTemplateService;
+    @Autowired
+    private OaProcessService oaProcessService;
+
+    @GetMapping("/findProcessType")
+    public Result findProcessType() {
+        return Result.ok(oaProcessTypeService.findProcessType());
+    }
+
+    @GetMapping("getProcessTemplate/{processTemplateId}")
+    public Result getProcessTemplate(@PathVariable Long processTemplateId) {
+        OaProcessTemplate oaProcessTemplate = oaProcessTemplateService.getById(processTemplateId);
+        return Result.ok(oaProcessTemplate);
+    }
+
+    @ApiOperation("启动流程")
+    @PostMapping("/startUp")
+    public Result startUp(@RequestBody ProcessFormVo processFormVo) {
+        oaProcessService.startUp(processFormVo);
+        return Result.ok();
+    }
+
+    @ApiOperation("待处理")
+    @GetMapping("/findPending/{page}/{limit}")
+    public Result findPending(@ApiParam(name = "page", value = "当前页码", required = true)
+                              @PathVariable Long page,
+                              @ApiParam(name = "limit", value = "每页记录数", required = true)
+                              @PathVariable Long limit) {
+        Page<OaProcess> pageParam = new Page<>(page, limit);
+        return Result.ok(oaProcessService.findPending(pageParam));
+    }
+
 }
