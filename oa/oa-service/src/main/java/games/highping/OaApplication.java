6f82d9cc8a1d870da34f74a59212e92e8adee17c
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2024-01-12 17:10:36 +0800

    Update OaApplication.java

--- oa/oa-service/src/main/java/games/highping/OaApplication.java
+++ oa/oa-service/src/main/java/games/highping/OaApplication.java
@@ -1,7 +1,9 @@
 package games.highping;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class OaApplication {
