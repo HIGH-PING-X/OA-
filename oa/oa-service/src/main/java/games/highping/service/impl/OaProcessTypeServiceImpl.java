a6ee2009cd95fd2d48dd46168d2bfd467e0fe689
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2024-01-12 17:10:12 +0800

    Update OaProcessTypeServiceImpl.java

--- oa/oa-service/src/main/java/games/highping/service/impl/OaProcessTypeServiceImpl.java
+++ oa/oa-service/src/main/java/games/highping/service/impl/OaProcessTypeServiceImpl.java
@@ -20,30 +20,30 @@ import java.util.List;
 @Service
 public class OaProcessTypeServiceImpl extends ServiceImpl<OaProcessTypeMapper, OaProcessType> implements OaProcessTypeService{
 
-//    @Autowired
-//    private OaProcessTemplateService processTemplateService;
-//
-//    //查询所有审批分类和每个分类所有审批模板
-//    @Override
-//    public List<OaProcessType> findProcessType() {
-//        //1 查询所有审批分类，返回list集合
-//        List<OaProcessType> processTypeList = baseMapper.selectList(null);
-//
-//        //2 遍历返回所有审批分类list集合
-//        for (OaProcessType processType:processTypeList) {
-//            //3 得到每个审批分类，根据审批分类id查询对应审批模板
-//            //审批分类id
-//            Long typeId = processType.getId();
-//            //根据审批分类id查询对应审批模板
-//            LambdaQueryWrapper<OaProcessTemplate> wrapper = new LambdaQueryWrapper<>();
-//            wrapper.eq(OaProcessTemplate::getProcessTypeId,typeId);
-//            List<OaProcessTemplate> processTemplateList = processTemplateService.list(wrapper);
-//
-//            //4 根据审批分类id查询对应审批模板数据（List）封装到每个审批分类对象里面
-//            processType.setProcessTemplateList(processTemplateList);
-//        }
-//        return processTypeList;
-//    }
+    @Autowired
+    private OaProcessTemplateService processTemplateService;
+
+    //查询所有审批分类和每个分类所有审批模板
+    @Override
+    public List<OaProcessType> findProcessType() {
+        //1 查询所有审批分类，返回list集合
+        List<OaProcessType> processTypeList = baseMapper.selectList(null);
+
+        //2 遍历返回所有审批分类list集合
+        for (OaProcessType processType:processTypeList) {
+            //3 得到每个审批分类，根据审批分类id查询对应审批模板
+            //审批分类id
+            Long typeId = processType.getId();
+            //根据审批分类id查询对应审批模板
+            LambdaQueryWrapper<OaProcessTemplate> wrapper = new LambdaQueryWrapper<>();
+            wrapper.eq(OaProcessTemplate::getProcessTypeId,typeId);
+            List<OaProcessTemplate> processTemplateList = processTemplateService.list(wrapper);
+
+            //4 根据审批分类id查询对应审批模板数据（List）封装到每个审批分类对象里面
+            processType.setProcessTemplateList(processTemplateList);
+        }
+        return processTypeList;
+    }
 
 }
 
